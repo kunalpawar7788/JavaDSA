d3f24c3d5103f28137bd5108cc16e180c9f30a49
kunalpawar7788 <kunal.pawar.137788@gmail.com>
2023-05-15 23:14:23 +0530

    Best time To buy and sell stock code merged!

---
+++ Arrays/src/com/kunal/BestTimeToTradeStock.java
@@ -0,0 +1,22 @@
+package com.kunal;
+public class BestTimeToTradeStock {
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(bestStock(prices));
+    }
+    public static int bestStock(int[] prices) {
+        if (prices.length == 1)
+            return 0;
+
+        int minBuy = prices[0];
+        int maxProfit = 0;
+        int profit;
+
+        for (int price: prices) {
+            minBuy = Math.min(minBuy,price);
+            profit = price - minBuy;
+            maxProfit = Math.max(maxProfit,profit);
+        }
+        return maxProfit;
+    }
+}
